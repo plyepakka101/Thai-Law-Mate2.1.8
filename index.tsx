f7e01d68ae7f9ae600b54ff92c2980c248fd7baf
pramot <108668748+plyepakka101@users.noreply.github.com>
2026-09-14 10:53:05 +0700

    fix: run legacy ID migration before Neon bootstrap

--- index.tsx
+++ index.tsx
@@ -1,7 +1,11 @@
+import './services/legacyIdMigration';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './AppV3';
 import { bootstrapNeonData } from './services/neonBootstrap';
+import { migrateLegacyCustomIds } from './services/legacyIdMigration';
+
+migrateLegacyCustomIds();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Could not find root element to mount to');
